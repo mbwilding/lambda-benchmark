c88f525bc8a363972b7d8cdf8ff93dba72d62f7a
Matthew Wilding <mbwilding@gmail.com>
2023-10-07 01:17:14 +0800

    Adjusted build script

--- runtimes/rust/build.sh
+++ runtimes/rust/build.sh
@@ -6,6 +6,6 @@ zip="code_${path}_${ARCH}.zip"
 
 rm ${zip} 2> /dev/null
 
-docker build . --build-arg ARCH=${ARCH} -t mbwilding/rust
+docker build . --build-arg ARCH="${ARCH/_/-}" -t mbwilding/rust
 dockerId=$(docker create mbwilding/rust)
 docker cp $dockerId:/code.zip ${zip}
